a4531cecb748c8642fc64b1af543918723c84a9a
Nicolás Domingo Morelli <159024539+nicolas-morelli@users.noreply.github.com>
2024-09-12 18:58:52 -0300

    tag change

--- app/api.py
+++ app/api.py
@@ -47,31 +47,58 @@ def process_track(i, tracks, name):
         return None
 
 
+def process_albums(name, key, argartists, index):
+    albums = requests.get(f'https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist={name}&api_key={key}&format=json')
+    albumlist = []
+
+    for i in range(0, len(albums.json()['topalbums']['album'])):
+        albumlist.append(process_album(i, albums, argartists.loc[index, 'name']))
+
+    return albumlist
+
+
+def process_album(i, albums, name):
+    try:
+        album = {col: albums.json()['topalbums']['album'][i][col] for col in albums.json()['topalbums']['album'][i] if col in ('name', 'playcount')}
+
+        album['stats_date'] = datetime.now().strftime('%Y-%m-%d')  # TODO: No usar, usar el del context de Airflow
+        album['artist'] = name
+
+        return album
+
+    except Exception:
+        return None
+
+
 def main():
     # Leo credenciales de Last.fm de JSON
     with open('creds.json', 'r') as creds:
         creds = json.load(creds)
         key = creds.get('key')
 
-    # Obtengo TOP 50 Argentina
-    argartists = pd.DataFrame(requests.get(f'https://ws.audioscrobbler.com/2.0/?method=tag.getTopArtists&tag=argentina&api_key={key}&format=json').json()['topartists']['artist'])[['name', 'url', 'mbid']].reset_index(names='rank')
-    argartists['rank'] = argartists['rank'] + 1
+    tag = 'heavy+metal'
+
+    tagartists = pd.DataFrame(requests.get(f'https://ws.audioscrobbler.com/2.0/?method=tag.getTopArtists&tag={tag}&api_key={key}&format=json').json()['topartists']['artist'])[['name', 'url', 'mbid']].reset_index(names='rank')
+    tagartists['rank'] = tagartists['rank'] + 1
 
     artistfact = []
+    albumbsfact = []
     tracksfact = []
-    for index, artist in argartists.iterrows():
+
+    for index, artist in tagartists.iterrows():
         name = artist['name'].replace('&', '').replace(' ', '+')
 
-        artistfact.append(process_artist(name, key, argartists, index))
-        tracksfact.append(process_tracks(name, key, argartists, index))
+        artistfact.append(process_artist(name, key, tagartists, index))
+        albumbsfact.append(process_albums(name, key, tagartists, index))
+        tracksfact.append(process_tracks(name, key, tagartists, index))
 
     artistfact = pd.DataFrame(artistfact)
+    albumbsfact = list(chain(*albumbsfact))
+    albumbsfact = pd.DataFrame(albumbsfact)
     tracksfact = list(chain(*tracksfact))
     tracksfact = pd.DataFrame(tracksfact)
 
-    print(tracksfact)
-
-    # TODO: Sacar top albums
+    print(artistfact)
 
     # TODO: Actualizar DIM con top tema, top album, top rank, nuevos valores de listeners y reproducciones
 
